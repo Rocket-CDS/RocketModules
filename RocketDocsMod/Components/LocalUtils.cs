3e270b1df83f1355c3907ee400e8a7e19a714950
leedavi <dcl@agence-sesame.fr>
2023-05-03 07:07:05 +0200

    RocketDocsMod

---
+++ RocketDocsMod/Components/LocalUtils.cs
@@ -0,0 +1,57 @@
+﻿using DNNrocketAPI.Components;
+using DotNetNuke.Common;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.Common.Utilities.Internal;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Portals;
+using DotNetNuke.Entities.Users;
+using DotNetNuke.Security;
+using DotNetNuke.Security.Permissions;
+using DotNetNuke.Services.Localization;
+using Newtonsoft.Json.Linq;
+using RazorEngine;
+using RazorEngine.Configuration;
+using RazorEngine.Templating;
+using Simplisity;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RocketDocsMod
+{
+    public class LocalUtils
+    {
+        public static void IncludeTextInHeader(Page page, string TextToInclude)
+        {
+            if (TextToInclude != "") page.Header.Controls.Add(new LiteralControl(TextToInclude));
+        }
+        public static void IncludeTextInHeaderAt(Page page, string TextToInclude, int addAt = 0)
+        {
+            if (addAt == 0) addAt = page.Header.Controls.Count;
+            if (TextToInclude != "") page.Header.Controls.AddAt(addAt, new LiteralControl(TextToInclude));
+        }
+        public static string GetLocalizeString(string keyName, string resourceFile = "")
+        {
+            if (resourceFile == "") resourceFile = "/DesktopModules/RocketModules/RocketDocsMod/App_LocalResources/Mod.ascx.resx";
+            return Localization.GetString(keyName, resourceFile);
+        }
+        public static string NavigateURL(int tabId, string[] param)
+        {
+            return Globals.NavigateURL(tabId, "", param).ToString();
+        }
+        public static string ReadTemplate(string template)
+        {
+            var razorTemplateFileMapPath = DNNrocketUtils.MapPath("/DesktopModules/RocketModules/RocketDocsMod/Themes/config-w3/1.0/default/" + template);
+            return FileSystemUtils.ReadFile(razorTemplateFileMapPath);
+        }
+    }
+}
---
+++ RocketDocsMod/View.ascx.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Web.UI.WebControls;
+using System.Xml;
+using DotNetNuke.Common;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Modules.Actions;
+using DotNetNuke.Security;
+using DotNetNuke.Services.Exceptions;
+using Newtonsoft.Json;
+using DotNetNuke.Common.Utilities;
+using Simplisity;
+using DotNetNuke.Entities.Users;
+using System.Linq;
+using System.Text;
+using DotNetNuke.Framework;
+using DotNetNuke.Framework.JavaScriptLibraries;
+using DNNrocketAPI.Components;
+using System.Web.UI;
+using RazorEngine.Templating;
+using System.Runtime.InteropServices.ComTypes;
+using RocketDocs.Components;
+using System.Collections.Generic;
+
+namespace RocketDocsMod
+{
+
+    /// -----------------------------------------------------------------------------
+    /// <summary>
+    /// The ViewNBrightGen class displays the content
+    /// </summary>
+    /// -----------------------------------------------------------------------------
+    public partial class View : PortalModuleBase, IActionable
+    {
+        #region Event Handlers
+
+        private bool _hasEditAccess;
+        public string ModuleLabel { get; set; }
+        
+        protected override void OnInit(EventArgs e)
+        {
+            try
+            {
+
+                base.OnInit(e);
+
+                _hasEditAccess = false;
+                if (UserId > 0) _hasEditAccess = DotNetNuke.Security.Permissions.ModulePermissionController.CanEditModuleContent(this.ModuleConfiguration);
+
+
+                var strHeader1 = LocalUtils.ReadTemplate("viewlastheader.cshtml");
+                PageIncludes.IncludeTextInHeader(Page, strHeader1);
+
+                var strHeader2 = LocalUtils.ReadTemplate("viewfirstheader.cshtml");
+                PageIncludes.IncludeTextInHeaderAt(Page, strHeader2, 0); 
+
+            }
+            catch (Exception ex)
+            {
+                Exceptions.ProcessModuleLoadException(this, ex);
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            try
+            {
+
+                base.OnLoad(e);
+
+                if (Page.IsPostBack == false)
+                {
+                    PageLoad();
+                }
+            }
+            catch (Exception exc) //Module failed to load
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+        private void PageLoad()
+        {
+            //var basePage = (DotNetNuke.Framework.CDefault)this.Page;
+            //if (!String.IsNullOrWhiteSpace(metaSEO.Title)) basePage.Title = metaSEO.Title;
+            //if (!String.IsNullOrWhiteSpace(metaSEO.Description)) basePage.MetaDescription = metaSEO.Description;
+            //if (!String.IsNullOrWhiteSpace(metaSEO.KeyWords)) basePage.MetaKeywords = metaSEO.KeyWords;
+        }
+        protected override void OnPreRender(EventArgs e)
+        {
+            var portalContent = new PortalContentLimpet(PortalId, DNNrocketUtils.GetCurrentCulture());             
+            var strOut = "";
+            var articleData = new ArticleLimpet(PortalId, ModuleId.ToString(), DNNrocketUtils.GetCurrentCulture());
+            var razorTempl = LocalUtils.ReadTemplate("view.cshtml");
+            var passSettings = new Dictionary<string, string>();
+            passSettings.Add("hasedit", _hasEditAccess.ToString());
+            passSettings.Add("moduleid", ModuleId.ToString());
+
+            var pr = RenderRazorUtils.RazorProcessData(razorTempl, articleData, null, passSettings, null, true);
+            if (pr.StatusCode != "00")
+                strOut = pr.ErrorMsg;
+            else
+                strOut = pr.RenderedText;
+
+            
+            // inject jQuery from DNN, to stop conflict with header.
+            JavaScript.RequestRegistration(CommonJs.jQuery);
+
+            var lit = new Literal();
+            lit.Text = strOut;
+            phData.Controls.Add(lit);
+
+        }
+
+
+        #endregion
+
+
+        #region Optional Interfaces
+
+        /// <summary>
+        /// The ModuleActions builds the module menu, for actions available.
+        /// </summary>
+        public ModuleActionCollection ModuleActions
+        {
+            get
+            {
+                var actions = new ModuleActionCollection();
+                return actions;
+            }
+        }
+
+        #endregion
+
+
+
+    }
+
+}
