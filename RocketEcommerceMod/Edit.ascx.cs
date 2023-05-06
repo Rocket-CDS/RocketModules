9e6fcbebe632216834fb591e8a4eca83952fd865
leedavi <dcl@agence-sesame.fr>
2023-05-06 10:46:53 +0200

    RocketEcommerceMod init

---
+++ RocketEcommerceMod/Edit.ascx.cs
@@ -0,0 +1,92 @@
+﻿using DNNrocketAPI.Components;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Services.Exceptions;
+using RocketEcommerceAPI.Components;
+using RocketPortal.Components;
+using Simplisity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RocketEcommerceMod
+{
+    public partial class Edit : ModuleSettingsBase
+    {
+        private string _systemkey;
+        private string _moduleRef;
+        private string _articleId;
+        private SessionParams _sessionParam;
+        protected override void OnInit(EventArgs e)
+        {
+            try
+            {
+                base.OnInit(e);
+                // Get systemkey from module name. (remove mod, add "API")
+                var moduleName = base.ModuleConfiguration.DesktopModule.ModuleName;
+                _systemkey = moduleName.ToLower().Substring(0, moduleName.Length - 3) + "api";
+                _articleId = DNNrocketUtils.RequestParam(Context, "articleid");
+                string skinSrcAdmin = "?SkinSrc=%2fDesktopModules%2fDNNrocket%2fRocketPortal%2fSkins%2frocketportal%2frocketedit";
+                if (DNNrocketUtils.RequestParam(Context, "SkinSrc") == "")
+                {
+                    if (_articleId == null || _articleId == "")
+                        Response.Redirect(EditUrl() + skinSrcAdmin, false);
+                    else
+                        Response.Redirect(EditUrl("articleid", _articleId) + skinSrcAdmin, false);
+                    Context.ApplicationInstance.CompleteRequest(); // do this to stop iis throwing error
+                }
+
+                _moduleRef = PortalId + "_ModuleID_" + ModuleId;
+
+                _sessionParam = new SessionParams(new SimplisityInfo());
+                _sessionParam.TabId = TabId;
+                _sessionParam.ModuleId = ModuleId;
+                _sessionParam.ModuleRef = _moduleRef;
+                _sessionParam.Set("articleid", _articleId);
+                _sessionParam.Set("moduleedit", "True");
+
+                //var strHeader1 = RocketDirectoryAPIUtils.DisplayAdminView(PortalId, _moduleRef, "", _sessionParam, "adminfirstheader.cshtml");
+                //PageIncludes.IncludeTextInHeader(Page, strHeader1);
+
+                //var strHeader2 = RocketDirectoryAPIUtils.DisplayAdminView(PortalId, _moduleRef, "", _sessionParam, "adminlastheader.cshtml");
+                //PageIncludes.IncludeTextInHeaderAt(Page, strHeader2, 0);
+
+            }
+            catch (Exception ex)
+            {
+                Exceptions.ProcessModuleLoadException(this, ex);
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            if (Page.IsPostBack == false)
+            {
+                PageLoad();
+            }
+        }
+
+        private void PageLoad()
+        {
+            try
+            {
+                var strOut = RocketEcommerceAPIUtils.DisplaySystemView(PortalId, _systemkey, _moduleRef, _sessionParam, "AdminDetailLoad.cshtml");
+                var lit = new Literal();
+                lit.Text = strOut;
+                phData.Controls.Add(lit);
+            }
+            catch (Exception exc)
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+    }
+}
---
+++ RocketEcommerceMod/Settings.ascx.cs
@@ -0,0 +1,79 @@
+﻿using DNNrocketAPI.Components;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.UI.UserControls;
+using Newtonsoft.Json;
+using RazorEngine;
+using RocketEcommerceAPI.Components;
+using RocketPortal.Components;
+using Simplisity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RocketEcommerceMod
+{
+    public partial class Settings : ModuleSettingsBase
+    {
+        private string _systemkey;
+        private string _moduleRef;
+        protected override void OnInit(EventArgs e)
+        {
+            try
+            {
+                base.OnInit(e);
+                // Get systemkey from module name. (remove mod, add "API")
+                var moduleName = base.ModuleConfiguration.DesktopModule.ModuleName;
+                _systemkey = moduleName.ToLower().Substring(0, moduleName.Length - 3) + "api";
+
+                _moduleRef = PortalId + "_ModuleID_" + ModuleId;
+            }
+            catch (Exception ex)
+            {
+                Exceptions.ProcessModuleLoadException(this, ex);
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            if (Page.IsPostBack == false)
+            {
+                PageLoad();
+            }
+        }
+
+        private void PageLoad()
+        {
+            try
+            {
+                var sessionParam = new SessionParams(new SimplisityInfo());
+                sessionParam.TabId = TabId;
+                sessionParam.ModuleId = ModuleId;
+                sessionParam.ModuleRef = _moduleRef;
+                var lang = "";
+                if (HttpContext.Current.Request.QueryString["language"] != null) lang = HttpContext.Current.Request.QueryString["language"];
+                DNNrocketUtils.SetCookieValue("simplisity_language", lang);
+
+                var strOut = RocketEcommerceAPIUtils.DisplaySystemView(PortalId, _systemkey, _moduleRef, sessionParam, "ModuleSettingsLoad.cshtml");
+
+                var lit = new Literal();
+                lit.Text = strOut;
+                phData.Controls.Add(lit);
+            }
+            catch (Exception exc)
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+    }
+}
