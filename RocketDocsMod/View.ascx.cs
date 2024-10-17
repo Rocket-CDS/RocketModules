cd97351c073e2316526444890543c850fe96ccc6
David Lee <dcl@agence-sesame.fr>
2024-10-17 16:49:51 +0200

    github path

--- RocketDocsMod/View.ascx.cs
+++ RocketDocsMod/View.ascx.cs
@@ -122,29 +122,49 @@ namespace RocketDocsMod
                     //var gitTree = GitHubUtils.GetGitHubTree("https://api.github.com/repos/Rocket-CDS/Documentation/git/trees/main?recursive=1");
 
                     var tabInfo = DNNrocketUtils.GetTabInfo(PortalId, TabId);
-                    var tabName = tabInfo.TabName;
-                    var urlTabName = tabName;
+                    var tabFile = tabInfo.TabName.Replace(" ", "").Replace(".md", "") + ".md";
+                    var tabName = tabInfo.TabName.Replace(" ", "");
+                    var parentTabName1 = "";
+                    var parentTabName2 = "";
+                    var parentTabName3 = "";
                     if (tabInfo.ParentId > 0)
                     {
                         var parentTabInfo = DNNrocketUtils.GetTabInfo(PortalId, tabInfo.ParentId);
-                        var parentTabName = parentTabInfo.TabName;
-                        urlTabName = parentTabName.Replace(" ", "");
+                        parentTabName1 = parentTabInfo.TabName.Replace(" ", "");
                         if (parentTabInfo.ParentId > 0)
                         {
                             var parentTabInfo2 = DNNrocketUtils.GetTabInfo(PortalId, parentTabInfo.ParentId);
-                            var parentTabName2 = parentTabInfo2.TabName;
-                            urlTabName = parentTabName2.Replace(" ", "") + "/" + parentTabName.Replace(" ", "");
+                            parentTabName2 = parentTabInfo2.TabName.Replace(" ", "");
                             if (parentTabInfo2.ParentId > 0)
                             {
                                 var parentTabInfo3 = DNNrocketUtils.GetTabInfo(PortalId, parentTabInfo2.ParentId);
-                                var parentTabName3 = parentTabInfo3.TabName;
-                                urlTabName = parentTabName3.Replace(" ", "") + "/" + parentTabName2.Replace(" ", "") + "/" + parentTabName.Replace(" ", "");
+                                parentTabName3 = parentTabInfo3.TabName.Replace(" ", "");
                             }
                         }
                     }
+                    var urlTabName1 = tabName + "/" + tabFile;
+                    var urlTabName2 = tabName + "/" + tabFile;
+                    if (parentTabName1 != "")
+                    {
+                        urlTabName1 = parentTabName1 + "/" + tabFile;
+                        urlTabName2 = parentTabName1 + "/" + tabName + "/" + tabFile;
+                    }
+                    if (parentTabName2 != "")
+                    {
+                        urlTabName1 = parentTabName2 + "/" + parentTabName1 + "/" + tabFile;
+                        urlTabName2 = parentTabName2 + "/" + parentTabName1 + "/" + tabName + "/" + tabFile;
+                    }
+                    if (parentTabName3 != "")
+                    {
+                        urlTabName1 = parentTabName3 + "/" + parentTabName2 + "/" + parentTabName1 + "/" + tabFile;
+                        urlTabName2 = parentTabName3 + "/" + parentTabName2 + "/" + parentTabName1 + "/" + tabName + "/" + tabFile;
+                    }
 
-                    var tokenUrl = portalContent.GitRawUserContentUrl + portalContent.GitHubRepo.TrimEnd('/') + "/refs/heads/main/" + urlTabName + "/" + tabName.Replace(" ", "").Replace(".md", "") + ".md";
-                    var mdtext2 = RocketDocsUtils.GetGitHubMarkdown(tokenUrl);
+
+                    var tokenUrl1 = portalContent.GitRawUserContentUrl + portalContent.GitHubRepo.TrimEnd('/') + "/refs/heads/main/" + urlTabName1;
+                    var tokenUrl2 = portalContent.GitRawUserContentUrl + portalContent.GitHubRepo.TrimEnd('/') + "/refs/heads/main/" + urlTabName2;
+                    var mdtext2 = RocketDocsUtils.GetGitHubMarkdown(tokenUrl1);
+                    if (mdtext2 == "FAIL") mdtext2 = RocketDocsUtils.GetGitHubMarkdown(tokenUrl2); // check for root document
                     if (mdtext2 == "FAIL") mdtext2 = "";
                     articleData.Info.SetXmlProperty("genxml/lang/genxml/textbox/summarykbase", mdtext2);
                 }
