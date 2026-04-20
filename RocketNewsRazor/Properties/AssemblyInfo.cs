277b712ab23f515eaa91f251b749ea702a5b12bd
leedavi <dcl@agence-sesame.fr>
2026-04-20 22:15:21 +0200

    RocketNewsRazor

---
+++ RocketNewsRazor/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RocketNewsRazor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RocketNewsRazor")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("b1c2d3e4-f5a6-7890-bcde-f01234567891")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
